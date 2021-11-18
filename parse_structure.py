64e70fe934ca6cddba496d988b85aa854187177b
Horace Pan <hopan@uchicago.edu>
2021-11-18 15:09:17 -0600

    add file for parsing 3d structure csv

---
+++ parse_structure.py
@@ -0,0 +1,114 @@
+import time
+import pdb
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from collections import namedtuple
+import torch
+import torch.nn.functional as F
+from torch_geometric.data import Data
+
+Bond = namedtuple("Bond", ['fro','to', 'type'])
+#ATOMIC_NUM = {'H': 1, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Br': 36, 'I': 53}
+ATOM_TYPES =  {'H': 0, 'C': 1, 'N': 2, 'O': 3, 'F': 4, 'Si':  5, 'P':  6, 'S':  7, 'Cl':  8, 'Br':  9, 'I': 10}
+
+def _get_edges(bonds):
+    '''
+    bonds: list of Bond named tuples
+    Returns: tuple of (list of ints, list of ints, list of ints)
+        1st list of ints: int id of head of bond
+        2nd list of ints: int id of tail of bond
+        3rd list of ints: bond types
+    Note: input bonds list is 1-indexed but the return is 0-indexed
+    '''
+    edges_from, edges_to = [], []
+    edge_types = []
+
+    for bond in bonds:
+        # sub 1 to get 0-indexed values
+        a1 = bond.fro - 1
+        a2 = bond.to - 1
+        bond_type = bond.type - 1
+
+        # add in both directions to make undirected
+        edges_from.append(a1)
+        edges_from.append(a2)
+        edges_to.append(a2)
+        edges_to.append(a1)
+        edge_types.append(bond_type)
+        edge_types.append(bond_type)
+
+    return edges_from, edges_to, edge_types
+
+def _get_atoms(atom_dict):
+    '''
+    atom_dict: dict mapping Atom string -> tuple of floats representing the atom's coordinates
+    Returns: tuple of atom types(list of ints) and coordinates (numpy array)
+    '''
+    atom_types = []
+    coords = np.zeros((len(atom_dict), 3))
+    idx = 0
+
+    for atom_str, xyz in atom_dict.items():
+        if type(atom_str) != str or 'Atom' not in atom_str:
+            continue
+        coords[idx] = xyz
+        idx += 1
+
+        # atom_str looks like: 'Atom(id, c)'
+        popen_idx =  atom_str.index('(') # index of open parens
+        comma_idx =  atom_str.index(',')
+        pclose_idx = atom_str.index(')')
+
+        atom_id = int(atom_str[popen_idx + 1: comma_idx]) - 1 # sub 1 to get 0 index
+        atom_type_str = atom_str[comma_idx + 1: pclose_idx].strip() # string rep of atom type
+        atom_types.append(atom_type_str)
+
+    return atom_types, coords
+
+def make_data(struc_str):
+    '''
+    struc_str: string that holds the atom, coordinate, and bond information
+    Returns: torch_geometric.data.Data object
+    '''
+    atom_dict = eval(struc_str)
+    bond_lst = atom_dict.pop('bonds')
+    bond_st, bond_end, bond_types = _get_edges(bond_lst)
+    atoms, coords = _get_atoms(atom_dict)
+    atoms = [ATOM_TYPES[a] for a in atoms]
+
+    atom_features = F.one_hot(torch.tensor(atoms), num_classes=len(ATOM_TYPES))
+    edge_index = torch.tensor([bond_st, bond_end], dtype=torch.long)
+    edge_attr = F.one_hot(torch.tensor(bond_types), num_classes=3)
+    pos = torch.tensor(coords)
+    data = Data(x=atom_features, edge_index=edge_index, edge_attr=edge_attr, pos=pos)
+    return data
+
+def parse_structure(fn):
+    '''
+    fn: string of location of the 3d_structure.csv file
+    Returns: list of torch_geometric.data.Data objects
+    '''
+    df = pd.read_csv(fn)
+    data_lst = []
+
+    for struc_str in tqdm(df['structure']):
+        atom_dict = eval(struc_str)
+        bond_lst = atom_dict.pop('bonds')
+
+        bond_st, bond_end, bond_types = _get_edges(bond_lst)
+        atoms, coords = _get_atoms(atom_dict)
+        atoms = [ATOM_TYPES[a] for a in atoms]
+
+        atom_features = F.one_hot(torch.tensor(atoms), num_classes=len(ATOM_TYPES))
+        edge_index = torch.tensor([bond_st, bond_end], dtype=torch.long)
+        edge_attr = F.one_hot(torch.tensor(bond_types), num_classes=3)
+        pos = torch.tensor(coords)
+        data = Data(x=atom_features, edge_index=edge_index, edge_attr=edge_attr, pos=pos)
+        data_lst.append(data)
+
+    return data_lst
+
+if __name__ == '__main__':
+    fn = './data/3d_struc.csv' # change location to wherever the file lives
+    data_lst = parse_structure(fn)
